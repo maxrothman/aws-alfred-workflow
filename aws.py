1b5d6aaf6abfe17de14539e595e049a69740b036
Max Rothman <max@edx.org>
2016-06-17 13:11:53 -0700

    Add caching for boto config

--- aws.py
+++ aws.py
@@ -6,24 +6,26 @@ import json, click, sys, time, uuid, os, pickle
 from datetime import datetime
 from numbers import Number
 
-EXPIRATION_WINDOW = 2   #seconds
+EXPIRATION_WINDOW = 2    #seconds
 CACHE_DIR = 'caches'
 CREDS_CACHE_FILE = os.path.join(CACHE_DIR, "creds.cache")
-CACHE_FILE_EXT = "aws-instances.cache"
-MAX_CACHE_AGE = 40      #seconds
+CONFIG_CACHE_FILE = os.path.join(CACHE_DIR, "boto-config.cache")
+INSTANCES_CACHE_EXT = "aws-instances.cache"
+INSTANCES_CACHE_MAX_AGE = 40    #seconds
 
 @click.group()
 def cli():
+  """Main group for other subcommands"""
   #Make sure cache dir exists
   if not os.path.exists(CACHE_DIR):
     os.mkdir(CACHE_DIR)
 
+#--------------------------------------------------------
 
 @cli.command()
 def get_profiles():
   """Print a alfred-formatted list of available boto profiles"""
-  from boto3 import Session   #Late import because importing boto3 is slow
-  profiles = Session()._session.full_config['profiles'].keys()
+  profiles = get_boto_config().keys()
   
   result = {
     "items": [
@@ -39,18 +41,18 @@ def get_profiles():
 
   click.echo(json.dumps(result))
 
+#--------------------------------------------------------
 
 @cli.command()
 @click.argument('profile')
 def check_profile(profile):
   """
-  If no MFA is necessary for "profile", exits with status 2
-  If an MFA is neccessary for "profile" but the cached temporary credentials are expired, exit with status 1
-  If an MFA is required for "profile" and the cached temporary credentials are still valid, exit with status 0
+  If no MFA is necessary for <profile>, exits with status 2
+  If an MFA is neccessary for <profile> but the cached temporary credentials are expired, exit with status 1
+  If an MFA is required for <profile> and the cached temporary credentials are still valid, exit with status 0
   """
-  from boto3 import Session   #Late import because importing boto3 is slow
-  
-  config = Session(profile_name=profile)._session.get_scoped_config()
+  config = get_boto_config([profile])
+
   if 'role_arn' not in config:
     sys.exit(2) #No MFA necessary, go straight to search
   
@@ -62,6 +64,7 @@ def check_profile(profile):
 
   sys.exit(0) #Creds are still valid, move along
 
+#--------------------------------------------------------
 
 @cli.command()
 @click.argument('profile')
@@ -86,98 +89,14 @@ def prompt_for_mfa(profile, token):
   }))
 
 
-@cli.command()
-@click.option('--profile')
-@click.argument('query')
-def search_for_instances(profile, query):
-  temp_creds = get_creds_cache(profile)
-  query = query.split()
-  result = {"items": []}
-
-  instances = get_instances(profile, temp_creds)
-
-  for instance in instances:
-    title = instance_title(instance)
-    subtitle = instance_subtitle(instance)
-    text = title + subtitle
-    match = 0
-
-    for q in query:
-      if q in text:
-        match += 1
-
-    if match == len(query):
-      entry = {
-        'uid': instance.id,
-        'title': title or '',       #Protect against potential None (unserializable)
-        'subtitle': subtitle or '',
-        'mods': {
-          'shift': {
-            'arg': json.dumps(extract_output_fields(instance)),
-            'subtitle': "More options",
-            'valid': True
-          }
-        }
-      }
-
-      # If the instance doesn't have a private IP address, the only valid action is "More options"
-      arg = {'arg': instance.private_ip_address} if getattr(instance, 'private_ip_address', False) else {'valid': False}
-      entry.update(arg)
-
-      result['items'].append(entry)
-
-  click.echo(json.dumps(result))
-
-
-@cli.command()
-@click.argument('spec')
-@click.argument('query')
-def filter_output_fields(spec, query):
-  """Filters on both title and subtitle, unlike default Alfred filtering, which filters only on title"""
-  spec = json.loads(spec)
-
-  results = {
-    "items": [
-      item for item in spec['items']
-      if query in item.get('title', '').lower() or query in item.get('subtitle', '').lower()
-    ]
-  }
-
-  click.echo(json.dumps(results))
-
-
-######## Helper functions ########
-def get_creds_cache(profile):
-  """Return the creds cache for a particular profile"""
-  if os.path.exists(CREDS_CACHE_FILE):
-    with open(CREDS_CACHE_FILE, 'r') as f:
-      return json.load(f)[profile]
-  else:
-    return None
-
-
-def update_creds_cache(profile, dct):
-  """Update the creds cache with <dct> as its new value"""
-  if os.path.exists(CREDS_CACHE_FILE):
-    with open(CREDS_CACHE_FILE, 'r') as f:
-      creds = json.load(f)
-    creds[profile] = dct
-    new_creds = creds
-  else:
-    new_creds = {profile: dct}
-
-  with open(CREDS_CACHE_FILE, 'w') as f:
-    json.dump(new_creds, f)
-
-
 def get_temp_creds(profile, token):
   """Use STS to retrieve temporary credentials for <profile>"""
+  from boto3 import Session   #Late import because importing boto3 is slow
+
   if len(token) != 6:
     return None
 
-  from boto3 import Session   #Late import because importing boto3 is slow
-  session = Session(profile_name=profile)
-  config = session._session.get_scoped_config()
+  config = get_boto_config()[profile]
 
   hub_client = Session(profile_name=config['source_profile']).client('sts')
 
@@ -202,10 +121,75 @@ def get_temp_creds(profile, token):
   }
 
 
+def update_creds_cache(profile, dct):
+  """Update the creds cache with <dct> as its new value"""
+  if os.path.exists(CREDS_CACHE_FILE):
+    with open(CREDS_CACHE_FILE, 'r') as f:
+      creds = json.load(f)
+    creds[profile] = dct
+    new_creds = creds
+  else:
+    new_creds = {profile: dct}
+
+  with open(CREDS_CACHE_FILE, 'w') as f:
+    json.dump(new_creds, f)
+
+#--------------------------------------------------------
+
+@cli.command()
+@click.option('--profile')
+@click.argument('query')
+def search_for_instances(profile, query):
+  """
+  Print an alfred-formatted list of instances in the AWS account given by <profile> that match <query>
+  """
+  temp_creds = get_creds_cache(profile)
+  query = query.split()
+  result = {"items": []}
+
+  instances = get_instances(profile, temp_creds)
+
+  for instance in instances:
+    title = instance_title(instance)
+    subtitle = instance_subtitle(instance)
+    text = title + subtitle
+    match = 0
+
+    for q in query:
+      if q in text:
+        match += 1
+
+    if match == len(query):
+      entry = {
+        'uid': instance.id,
+        'title': title or '',       #Protect against potential None (unserializable)
+        'subtitle': subtitle or '',
+        'mods': {
+          'shift': {
+            #Pass the selected result as a string to the next node, which filters it
+            'arg': json.dumps(extract_output_fields(instance)),
+            'subtitle': "More options",
+            'valid': True
+          }
+        }
+      }
+
+      # If the instance doesn't have a private IP address, the only valid action is "More options"
+      arg = ({'arg': instance.private_ip_address} 
+        if getattr(instance, 'private_ip_address', False) 
+        else {'valid': False})
+      entry.update(arg)
+
+      result['items'].append(entry)
+
+  click.echo(json.dumps(result))
+
+
 def get_instances(profile, temp_creds):
-  """Get a list of all instances in the account from AWS"""
-  # import pdb; pdb.set_trace()
-  cache_file = os.path.join(CACHE_DIR, profile + '-' + CACHE_FILE_EXT)
+  """Get a list of all instances in the account given by <profile> from AWS"""
+
+  cache_file = os.path.join(CACHE_DIR, profile + '-' + INSTANCES_CACHE_EXT)
+
   if temp_creds is not None:
     cred_kwargs = {
       'aws_access_key_id': temp_creds['access_key'],
@@ -215,7 +199,7 @@ def get_instances(profile, temp_creds):
   else:
     cred_kwargs = {}
 
-  if not os.path.isfile(cache_file) or os.stat(cache_file).st_mtime + MAX_CACHE_AGE < time.time():
+  if not os.path.isfile(cache_file) or os.stat(cache_file).st_mtime + INSTANCES_CACHE_MAX_AGE < time.time():
     from boto3 import Session  #Late import because importing boto3 is slow
 
     ec2 = Session(profile_name=profile, **cred_kwargs).resource('ec2')
@@ -225,9 +209,10 @@ def get_instances(profile, temp_creds):
       pickle.dump(instances, f, pickle.HIGHEST_PROTOCOL)
 
     return instances
-  
-  with open(cache_file) as f:
-    return pickle.load(f)
+
+  else:
+    with open(cache_file) as f:
+      return pickle.load(f)
 
 
 def instance_title(instance):
@@ -272,6 +257,65 @@ def extract_output_fields(instance):
     ]
   }
 
+#--------------------------------------------------------
+
+@cli.command()
+@click.argument('spec')
+@click.argument('query')
+def filter_output_fields(spec, query):
+  """Filters on both title and subtitle, unlike default Alfred filtering, which filters only on title"""
+  spec = json.loads(spec)
+
+  results = {
+    "items": [
+      item for item in spec['items']
+      if query in item.get('title', '').lower() or query in item.get('subtitle', '').lower()
+    ]
+  }
+
+  click.echo(json.dumps(results))
+
+
+######## Shared helper functions ########
+def get_creds_cache(profile):
+  """Return the creds cache for a particular profile"""
+  if os.path.exists(CREDS_CACHE_FILE):
+    with open(CREDS_CACHE_FILE, 'r') as f:
+      return json.load(f)[profile]
+  else:
+    return None
+
+
+def get_boto_config():
+  """Return full boto config. Caches responses for performance."""
+  cf_files = filter(os.path.exists, map(os.path.expanduser,
+    ['~/.aws/config', '~/.aws/credentials', '/etc/boto.cfg', '~/.boto']))
+  env_vars = ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_SESSION_TOKEN',
+    'AWS_DEFAULT_REGION', 'AWS_PROFILE', 'AWS_CONFIG_FILE', 'AWS_SHARED_CREDENTIALS_FILE',
+    'AWS_CA_BUNDLE', 'AWS_METADATA_SERVICE_TIMEOUT', 'AWS_METADATA_SERVICE_NUM_ATTEMPTS',
+    'AWS_DATA_PATH']
+
+  if os.path.exists(CONFIG_CACHE_FILE):
+    with open(CONFIG_CACHE_FILE) as f:
+      cache = json.load(f)
+
+    cache_invalid = (
+      any(os.stat(cf).st_mtime > os.stat(CONFIG_CACHE_FILE).st_mtime for cf in cf_files) or
+      any(os.environ.get(cv) != cache['env'].get(cv) for cv in env_vars)
+    )
+  else:
+    cache_invalid = True
+
+  if cache_invalid:
+    from boto3 import Session   #late import because importing boto3 is slow
+    config = Session()._session.full_config['profiles']
+    with open(CONFIG_CACHE_FILE, 'w') as f:
+      json.dump({'config': config, 'env': {cv: os.environ.get(cv) for cv in env_vars}}, f)
+    return config
+
+  else:
+    return cache['config']
+
 
 class SerializableInstance(object):
   """A wrapper for Boto3 Instance resources that is pickleable"""
@@ -280,7 +324,6 @@ class SerializableInstance(object):
       val = getattr(instance, prop)
       if self._is_serializable(val):
         setattr(self, prop, val)
-
 
   def _is_serializable(self, val):
     if isinstance(val, Number):
@@ -291,9 +334,17 @@ class SerializableInstance(object):
       return all(self._is_serializable(v) for v in val.values())
     elif isinstance(val, list):
       return all(self._is_serializable(i) for i in val)
-    return False
+    else:
+      return False
 
 
 
 if __name__ == '__main__':
   cli()
+
+#TODO:
+# - hooks to jump to any stage in the pipeline
+# - more feedback while doing mfa (error, too short, etc.)
+# - parameterize details (e.g. title/subtitle fields)
+# - performance
+# - add fuzzy matching
